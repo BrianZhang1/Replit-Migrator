970b25e2fcc7616f30ebaa064007dee639f9eebb
Brian Zhang <brian22ca@gmail.com>
2023-12-31 11:10:20 -0500

    Add line count and average lines to report.

--- replit_migrator/screens/report_screen.py
+++ replit_migrator/screens/report_screen.py
@@ -4,7 +4,6 @@ from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 import matplotlib.pyplot as plt
 import os
-from PIL import Image
 
 class ReportScreen:
     def __init__(self, root, data_handler):
@@ -39,14 +38,18 @@ class ReportScreen:
         self.pdf_canvas = canvas.Canvas('report.pdf', pagesize=letter)
 
         # Gather data.
-        file_type_count = self.count_file_types()
+        file_type_count, file_count = self.count_files_and_types()
+        total_lines = sum(file_type_count.values())
 
-        self.draw_text("Repl.it Report", font_size=20, line_spacing=10)
-        self.draw_text(f"Total projects: {len(self.data)}")
-        self.draw_text(f"Total lines of code: {sum(file_type_count.values())}")
-        self.draw_pie_chart(file_type_count.values(), file_type_count.keys(), "File Types", 4*inch)
+        self.draw_text('Repl.it Report', font_size=20, line_spacing=10)
+        self.draw_text(f'Total projects: {len(self.data)}')
+        self.draw_text(f'Total files: {file_count}')
+        self.draw_text(f'Total lines of code: {total_lines}')
+        self.draw_text(f'Average lines per file: {round(total_lines/file_count, 2)}')
+        self.draw_text('', font_size=0, line_spacing=10)
+        self.draw_pie_chart(file_type_count.values(), file_type_count.keys(), 'File Types', 4*inch)
         for file_type in file_type_count:
-            self.draw_text(f"Lines of {file_type}: {file_type_count[file_type]}")
+            self.draw_text(f'Lines of {file_type}: {file_type_count[file_type]}')
 
         # Save the PDF.
         self.pdf_canvas.save()
@@ -62,7 +65,7 @@ class ReportScreen:
         if y is None:
             y = self.line_begin
 
-        self.pdf_canvas.setFont("Helvetica", font_size)
+        self.pdf_canvas.setFont('Helvetica', font_size)
         actual_y = letter[1] - font_size - y
         self.pdf_canvas.drawString(x, actual_y, text)
 
@@ -96,12 +99,16 @@ class ReportScreen:
         os.remove('pie_chart.png')
 
 
-    def count_file_types(self):
-        """Counts the total number of lines of each file type, by file extension."""
+    def count_files_and_types(self):
+        """
+        Counts the total number of lines of each file type, by file extension,
+        as well as the total number of code files (e.g. not images).
+        """
 
         # Store the count of each file type in a dictionary.
         # Key: file extension, Value: count
-        count = dict()
+        type_count = dict()
+        code_file_count = 0
 
         # For each file, count the number of lines and add it to the count dictionary.
         search_dir = os.path.join(os.getcwd(), 'output/')
@@ -113,9 +120,10 @@ class ReportScreen:
                 if file_line_count == -1:
                     # Not a readable file, perhaps an image or binary file. Skip this file.
                     continue
-                count[extension] = count.get(extension, 0) + file_line_count
+                type_count[extension] = type_count.get(extension, 0) + file_line_count
+                code_file_count += 1
 
-        return count
+        return type_count, code_file_count
 
 
     def count_lines(self, file_path):
